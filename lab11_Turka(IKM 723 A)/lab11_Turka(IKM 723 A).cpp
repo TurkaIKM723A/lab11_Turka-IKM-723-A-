b4bd20794ab5d93da414ef21a866c17cbf90cebc
Jambi <iturkas42@gmail.com>
2024-04-07 16:10:27 +0300

    Завдання 3

--- lab11_Turka(IKM 723 A)/lab11_Turka(IKM 723 A).cpp
+++ lab11_Turka(IKM 723 A)/lab11_Turka(IKM 723 A).cpp
@@ -4,17 +4,50 @@ using namespace std;
 
 int main() {
 
-    setlocale(LC_CTYPE, "ukr"); 
+    setlocale(LC_CTYPE, "ukr");
 
-    char* ptr;
+    int n, m;
 
-    ptr = new char;
+    cout << "¬ведiть розмiрнiсть масиву x[n]: ";
+    cin >> n;
+    cout << "¬ведiть розмiрнiсть масиву y[m]: ";
+    cin >> m;
 
-    *ptr = 'A';
+    int* x = new int[n];
+    int* y = new int[m];
 
-    cout << "«наченн€, на €ке вказуЇ покажчик: " << *ptr << '\n';
+    cout << "¬ведiть елементи масиву x[n]: ";
 
-    delete ptr;
+    for (int i = 0; i < n; ++i) {
+
+        cin >> x[i];
+
+    }
+
+    cout << "¬ведiть елементи масиву y[m]: ";
+
+    for (int i = 0; i < m; ++i) {
+
+        cin >> y[i];
+
+    }
+
+    int first_element_x = x[0];
+    int count = 0;
+
+    for (int i = 0; i < m; ++i) {
+
+        if (y[i] == first_element_x) {
+
+            count++;
+
+        }
+    }
+
+    cout << "ѕерший елемент масиву x зустрiчаЇтьс€ в масивi y " << count << " разiв." << '\n';
+
+    delete[] x;
+    delete[] y;
 
     return 0;
 }
