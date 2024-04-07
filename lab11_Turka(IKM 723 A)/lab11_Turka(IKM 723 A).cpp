da43bbc2e763ec7bac381082ca940d475d0129e5
Jambi <iturkas42@gmail.com>
2024-04-07 15:52:38 +0300

    Завдання 2

--- lab11_Turka(IKM 723 A)/lab11_Turka(IKM 723 A).cpp
+++ lab11_Turka(IKM 723 A)/lab11_Turka(IKM 723 A).cpp
@@ -4,31 +4,18 @@ using namespace std;
 
 int main() {
 
-    int a, b, c;
+    setlocale(LC_CTYPE, "ukr"); 
 
-    setlocale(LC_CTYPE, "ukr");
+    char* ptr;
 
-    cout << "Введiть значення змiнної a: ";
-    cin >> a;
-    cout << "Введiть значення змiнної b: ";
-    cin >> b;
-    cout << "Введiть значення змiнної c: ";
-    cin >> c;
+    ptr = new char;
 
-    int* ptr_a = &a;
-    int* ptr_b = &b;
-    int* ptr_c = &c;
+    *ptr = 'A';
 
-    *ptr_a *= 2;
+    cout << "Значення, на яке вказує покажчик: " << *ptr << '\n';
 
-    int temp = *ptr_b;
-    *ptr_b = *ptr_c;
-    *ptr_c = temp;
+    delete ptr;
 
-    cout << " " << '\n';
-    cout << "a пiсля збiльшення в 2 рази: " << a << '\n';
-    cout << "b пiсля обмiну з c: " << b << '\n';
-    cout << "c пiсля обмiну з b: " << c << '\n';
-     
     return 0;
 }
+
